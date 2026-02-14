079dd28196979072d099e83e4602da10e0b83e8d
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2026-02-14 09:10:06 +0100

    First commit

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///recipes.db"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///recipes.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
