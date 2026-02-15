d2a4e68050d29522471f2a42a465c81d31b09f32
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2026-02-15 07:15:25 +0100

    Update requirements.txt

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ import os
 class Config:
     # SQLALCHEMY_DATABASE_URI = "sqlite:///recipes.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI :
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql+psycopg://")
 
