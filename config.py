fa184aa6dacdec1d39b0875af0e1222a54df2cda
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2026-02-16 05:59:52 +0100

    Update requirements.txt

--- config.py
+++ config.py
@@ -1,9 +1,23 @@
 import os
 class Config:
     # SQLALCHEMY_DATABASE_URI = "sqlite:///recipes.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI :
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql+psycopg://")
+    database_url = os.environ.get("DATABASE_URL")
 
+    if database_url:
+        if database_url.startswith("postgres://"):
+            database_url = database_url.replace(
+                "postgres://",
+                "postgresql+psycopg://",
+                1
+            )
+        elif database_url.startswith("postgresql://"):
+            database_url = database_url.replace(
+                "postgresql://",
+                "postgresql+psycopg://",
+                1
+            )
+
+    SQLALCHEMY_DATABASE_URI = database_url
